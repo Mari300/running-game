a9a60447597d595243662642c7538e6b254efcb3
Jahmari Maxwell <jahfly9@gmail.com>
2019-01-24 19:24:12 +0000

    Intial step completed. Person 'div' is moving with arrow keys

--- running-game.js
+++ running-game.js
@@ -1,14 +1,22 @@
-function myMove() {
-  var elem = document.getElementsByClassName("road");   
-  var pos = 0;
-  var id = setInterval(frame, 5);
-  function frame() {
-    if (pos == 350) {
-      clearInterval(id);
-    } else {
-      pos++; 
-      elem.style.top = pos + "px"; 
-      elem.style.bottom = pos + "px"; 
-    }
-  }
+$( document ).ready(function() {
+var guy = document.getElementById("person"); 
+var container = document.getElementById("container"); 
+var personLeft = 0; 
+var y = 0;  
+function go(e) { 
+if (e.keyCode == 39) { 
+personLeft += 2; guy.style.left = personLeft + "px"; 
+} 
+else if (e.keyCode == 37) {
+personLeft -= 2; guy.style.left = personLeft + "px"; 
 }
+else if (e.keyCode == 40) {
+y += 2; guy.style.top = y + "px"; 
+}
+else if (e.keyCode == 38) {
+y -= 2; guy.style.top = y + "px"; 
+}
+
+} document.onkeydown = go;
+
+})
