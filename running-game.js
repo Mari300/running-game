d4b8005611a68a71fbb9da40f5dcd95d37b9255e
Jahmari Maxwell <jahfly9@gmail.com>
2019-01-27 05:59:29 +0000

    trying to create a stage and position the person

--- running-game.js
+++ running-game.js
@@ -5,16 +5,16 @@ var personLeft = 0;
 var y = 0;  
 function go(e) { 
 if (e.keyCode == 39) { 
-personLeft += 2; guy.style.left = personLeft + "px"; 
+personLeft += 10; guy.style.left = personLeft + "px"; 
 } 
 else if (e.keyCode == 37) {
-personLeft -= 2; guy.style.left = personLeft + "px"; 
+personLeft -= 10; guy.style.left = personLeft + "px"; 
 }
 else if (e.keyCode == 40) {
-y += 2; guy.style.top = y + "px"; 
+y += 10; guy.style.top = y + "px"; 
 }
 else if (e.keyCode == 38) {
-y -= 2; guy.style.top = y + "px"; 
+y -= 10; guy.style.top = y + "px"; 
 }
 
 } document.onkeydown = go;
