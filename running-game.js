f7784ed04902a710a5b2a170ab260ece4cefeca0
Jahmari Maxwell <jahfly9@gmail.com>
2019-02-01 07:12:24 +0000

    learning about velocity and the DOM

--- running-game.js
+++ running-game.js
@@ -1,22 +1,117 @@
 $( document ).ready(function() {
-var guy = document.getElementById("person"); 
-var container = document.getElementById("container"); 
-var personLeft = 0; 
-var y = 0;  
-function go(e) { 
-if (e.keyCode == 39) { 
-personLeft += 10; guy.style.left = personLeft + "px"; 
-} 
-else if (e.keyCode == 37) {
-personLeft -= 10; guy.style.left = personLeft + "px"; 
-}
-else if (e.keyCode == 40) {
-y += 10; guy.style.top = y + "px"; 
-}
-else if (e.keyCode == 38) {
-y -= 10; guy.style.top = y + "px"; 
-}
+rectangle = document.getElementById("person")
+var context, controller, rectangle, loop;
 
-} document.onkeydown = go;
+context = document.querySelector("#container");
 
-})
+context.canvas.height = 180;
+context.canvas.width = 320;
+
+rectangle = {
+
+  height:32,
+  jumping:true,
+  width:32,
+  x:144, // center of the canvas
+  x_velocity:0,
+  y:0,
+  y_velocity:0
+
+};
+
+controller = {
+
+  left:false,
+  right:false,
+  up:false,
+  keyListener:function(event) {
+
+    var key_state = (event.type == "keydown")?true:false;
+
+    switch(event.keyCode) {
+
+      case 37:// left key
+        controller.left = key_state;
+      break;
+      case 38:// up key
+        controller.up = key_state;
+      break;
+      case 39:// right key
+        controller.right = key_state;
+      break;
+
+    }
+
+  }
+
+};
+
+loop = function() {
+
+  if (controller.up && rectangle.jumping == false) {
+
+    rectangle.y_velocity -= 20;
+    rectangle.jumping = true;
+
+  }
+
+  if (controller.left) {
+
+    rectangle.x_velocity -= 0.5;
+
+  }
+
+  if (controller.right) {
+
+    rectangle.x_velocity += 0.5;
+
+  }
+
+  rectangle.y_velocity += 1.5;// gravity
+  rectangle.x += rectangle.x_velocity;
+  rectangle.y += rectangle.y_velocity;
+  rectangle.x_velocity *= 0.9;// friction
+  rectangle.y_velocity *= 0.9;// friction
+
+  // if rectangle is falling below floor line
+  if (rectangle.y > 180 - 16 - 32) {
+
+    rectangle.jumping = false;
+    rectangle.y = 180 - 16 - 32;
+    rectangle.y_velocity = 0;
+
+  }
+
+  // if rectangle is going off the left of the screen
+  if (rectangle.x < -32) {
+
+    rectangle.x = 320;
+
+  } else if (rectangle.x > 320) {// if rectangle goes past right boundary
+
+    rectangle.x = -32;
+
+  }
+
+  context.fillStyle = "#202020";
+  context.fillRect(0, 0, 320, 180);// x, y, width, height
+  context.fillStyle = "#ff0000";// hex for red
+  context.beginPath();
+  context.rect(rectangle.x, rectangle.y, rectangle.width, rectangle.height);
+  context.fill();
+  context.strokeStyle = "#202830";
+  context.lineWidth = 4;
+  context.beginPath();
+  context.moveTo(0, 164);
+  context.lineTo(320, 164);
+  context.stroke();
+
+  // call update when the browser is ready to draw again
+  window.requestAnimationFrame(loop);
+
+};
+
+window.addEventListener("keydown", controller.keyListener);
+window.addEventListener("keyup", controller.keyListener);
+window.requestAnimationFrame(loop);
+)};
